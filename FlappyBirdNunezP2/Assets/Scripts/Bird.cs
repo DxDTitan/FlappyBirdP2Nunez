77ee885d4e6a3672b97f3769b2f15598f7690253
DxDTitan <741865@student.dusd.net>
2024-12-13 09:56:50 -0800

    Finished Scripts
    Finished the scripts, will replace columns with a different image

--- FlappyBirdNunezP2/Assets/Scripts/Bird.cs
+++ FlappyBirdNunezP2/Assets/Scripts/Bird.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Bird : MonoBehaviour
 {
     public float upForce = 200f;
 
@@ -12,8 +12,8 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D> ();
-        anim = GetComponent<Animator> ();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,19 +21,20 @@ public class Movement : MonoBehaviour
     {
         if (isDead == false)
         {
-            if(Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
-                anim.SetTrigger ("Flap");
+                anim.SetTrigger("Flap");
             }
         }
     }
 
-    void OnCollisionEnter2D () 
+    void OnCollisionEnter2D()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
-        anim.SetTrigger  ("Die");
-        GameControl.instance.BirdDied (); 
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
